16c00fa19b6b29c803df8391bee3d8169d7a894e
Salton Massally <salton.massally@gmail.com>
2014-04-13 02:48:57 +0000

    added kannel recipe declaration to kannel metadata.rb and removed several unneeded file from the cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             "careers.sl"
+name             "kannel"
 maintainer       "Salton Massally"
 maintainer_email "salton.massally@gmail.com"
 license          "MIT"
-description      "Installs/Configures the careers.sl platform"
+description      "Installs/Configures Kannel - heavily based on https://github.com/krishnenc/chef-kannel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
