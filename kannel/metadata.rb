329f5c317a063651b6120e93cb01f3ac73a21993
Salton Massally <salton.massally@gmail.com>
2014-04-13 01:54:45 +0000

    removed postgres inclusion from kannel cookbook berksfile and removed apache2 dependency from the same project

--- kannel/metadata.rb
+++ kannel/metadata.rb
@@ -6,8 +6,6 @@ description      "Installs/Configures Kannel - heavily based on https://github.c
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends "apache2", "~> 1.4.0"
-
 %w[ubuntu].each do |os|
   supports os
 end
