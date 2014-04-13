db92e854f5a0f1db447438c08791cde43378981f
Salton Massally <salton.massally@gmail.com>
2014-04-13 16:51:34 +0000

    fix to <Chef::Exceptions::InvalidRemoteFileUR

--- drupal-solr/recipes/default.rb
+++ drupal-solr/recipes/default.rb
@@ -16,19 +16,20 @@ remote_directory node['drupal-solr']['tomcat_lib_dir']  do
   files_owner node['drupal-solr']['tomcat_user']
   mode "755"
   owner node['drupal-solr']['tomcat_user']
-  source "/usr/local/solr-#{node['drupal-solr']['solr_version']}/dist/solrj-lib"
+  source "file://usr/local/solr-#{node['drupal-solr']['solr_version']}/dist/solrj-lib"
   group node['drupal-solr']['tomcat_group']
 end
 
+
 remote_file "#{node['drupal-solr']['tomcat_conf_dir']}/log4j.properties" do 
-  source "/usr/local/solr-#{node['drupal-solr']['solr_version']}/example/resources/log4j.properties"
+  source "file://usr/local/solr-#{node['drupal-solr']['solr_version']}/example/resources/log4j.properties"
   owner node['drupal-solr']['tomcat_user']
   group node['drupal-solr']['tomcat_group']
   mode 0755
 end
 
 remote_file "#{node['drupal-solr']['tomcat_webapp_dir']}/solr.war" do 
-  source "/usr/local/solr-#{node['drupal-solr']['solr_version']}/dist/solr-#{node['drupal-solr']['solr_version']}.war "
+  source "file://usr/local/solr-#{node['drupal-solr']['solr_version']}/dist/solr-#{node['drupal-solr']['solr_version']}.war "
   owner node['drupal-solr']['tomcat_user']
   group node['drupal-solr']['tomcat_group']
   mode 0755
@@ -47,7 +48,7 @@ remote_directory node['drupal-solr']['tomcat_lib_dir']  do
   files_owner node['drupal-solr']['tomcat_user']
   mode "755"
   owner node['drupal-solr']['tomcat_user']
-  source "/usr/local/solr-#{node['drupal-solr']['solr_version']}/example/solr/collection1/conf"
+  source "file://usr/local/solr-#{node['drupal-solr']['solr_version']}/example/solr/collection1/conf"
   group node['drupal-solr']['tomcat_group']
 end
 
@@ -71,7 +72,7 @@ remote_directory "#{node['drupal-solr']['solr_home']}/drupal}"  do
   files_owner node['drupal-solr']['tomcat_user']
   mode "755"
   owner node['drupal-solr']['tomcat_user']
-  source "#{node['drupal-solr']['solr_home']}/conf"
+  source "file://#{node['drupal-solr']['solr_home']}/conf"
   group node['drupal-solr']['tomcat_group']
 end
 
