60119afcf4ff7bfb2777c3b1e11a80486cbb479c
Salton Massally <salton.massally@gmail.com>
2014-04-16 16:39:33 +0000

    temp hack to solve You must supply a name when declaring a template resource

--- postfix-ses/attributes/default.rb
+++ postfix-ses/attributes/default.rb
@@ -7,6 +7,9 @@ override['postfix']['main']['relayhost'] = 'email-smtp.us-east-1.amazonaws.com:2
 override['postfix']['main']['mynetworks'] = '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128'
 override['postfix']['sasl']['smtp_sasl_user_name'] = 'user'
 override['postfix']['sasl']['smtp_sasl_passwd'] = 'secret'
-override['postfix']['main']['smtp_tls_security_level'] = 'encrypt'
-override['postfix']['main']['smtp_tls_note_starttls_offer'] = 'yes'
+default['postfix']['main']['smtp_tls_security_level'] = 'encrypt'
+default['postfix']['main']['smtp_tls_note_starttls_offer'] = 'yes'
 
+default['postfix']['sasl_password_file'] = "#{node['postfix']['conf_dir']}/sasl_passwd"
+default['postfix']['main']['smtp_sasl_password_maps'] = "hash:#{node['postfix']['sasl_password_file']}"
+default['postfix']['main']['smtp_sasl_security_options'] = 'noanonymous'
