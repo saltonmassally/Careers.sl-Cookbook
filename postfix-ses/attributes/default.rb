3a1f0a22215e8760c9dec4c0ab5edbf86cb97e26
Salton Massally <salton.massally@gmail.com>
2014-04-19 21:18:51 +0000

    can't seem to be able to override the username and password attributes in postfix so remove the one overridden in my own implementation to see if that fixes it

--- postfix-ses/attributes/default.rb
+++ postfix-ses/attributes/default.rb
@@ -5,10 +5,8 @@ override['postfix']['main']['mydestination'] = ''
 override['postfix']['main']['smtp_sasl_auth_enable'] = 'yes'
 override['postfix']['main']['relayhost'] = 'email-smtp.us-east-1.amazonaws.com:25'
 override['postfix']['main']['mynetworks'] = '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128'
-override['postfix']['sasl']['smtp_sasl_user_name'] = 'user'
-override['postfix']['sasl']['smtp_sasl_passwd'] = 'secret'
-default['postfix']['main']['smtp_tls_security_level'] = 'encrypt'
-default['postfix']['main']['smtp_tls_note_starttls_offer'] = 'yes'
+override['postfix']['main']['smtp_tls_security_level'] = 'encrypt'
+override['postfix']['main']['smtp_tls_note_starttls_offer'] = 'yes'
 
 default['postfix']['sasl_password_file'] = "#{node['postfix']['conf_dir']}/sasl_passwd"
 default['postfix']['main']['smtp_sasl_password_maps'] = "hash:#{node['postfix']['sasl_password_file']}"
