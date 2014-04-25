c079f2c2bcf3ea2903c6530892e350fd8aa0ce18
Salton Massally <salton.massally@gmail.com>
2014-04-25 18:19:12 +0000

    trying out a new cookbook for installing solr

--- smssuite/recipes/deploy.rb
+++ smssuite/recipes/deploy.rb
@@ -1,8 +1,36 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |deploy_application, deploy|
+   if deploy[:application_type] != 'other'
+     Chef::Log.debug("Skipping deploy::other application #{application} as it is not an other app")
+     next
+   end
 
+   bash "install_requirements" do
+    code <<-EOH
+    pip install -r #{deploy[:absolute_document_root]}#{node[:smssuite][:requirement_file]}
+    EOH
+   end
 
+  template node[:smssuite][:settings_file] do
+    source "settings.py.erb"
+    owner deploy[:user]
+    group deploy[:group]
+    mode "0644"
+    action :create
+  end
+
+  bash "syncdb" do
+    code <<-EOH
+    python #{deploy[:absolute_document_root]}manage.py syncdb
+    EOH
+  end
+
+  bash "migratedb" do
+    code <<-EOH
+    python #{deploy[:absolute_document_root]}manage.py migrate
+    EOH
+  end
 
 end
 
---
+++ solr-drupal/attributes/default.rb
@@ -0,0 +1,10 @@
+override['solr']['version'] = '4.7.2'
+override['solr']['checksum'] = ''
+
+override['jetty']['port'] = 8080
+override['java']['jdk_version'] = 7
+
+override['jetty']['version'] = '9.0.3.v20130506'
+override['jetty']['link'] = 'http://eclipse.org/downloads/download.php?file=/jetty/stable-9/dist/jetty-distribution-9.1.4.v20140401.tar.gz&r=1'
+override['jetty']['checksum'] = ''
+
